7d25e11f4656d2aac6c58e7ac28829faa6e2e7a0
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2023-08-30 18:51:08 -0300

    Estilizando resume

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -1,4 +1,7 @@
 <script>
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
+
 export default {
     name: 'Resume',
 }
@@ -7,9 +10,41 @@ export default {
 <template>
     <div class="container">
         <div class="content">
-            <div class="card-resume">Entradas</div>
-            <div class="card-resume">Saidas</div>
-            <div class="card-resume">Total</div>
+            <div class="card-resume">
+                <div class="card-resume-title">
+                    <span>Entradas</span>
+                    <div class='icon'>
+                        <i class="fas fa-arrow-up"></i>
+                    </div>
+                </div>
+                <div class="card-resume-values">
+                    <span>R$ 1000</span>
+                </div>
+            </div>
+
+            <div class="card-resume">
+                <div class="card-resume-title">
+                    <span>Entradas</span>
+                    <div class='icon'>
+                        <i class="fas fa-arrow-down"></i>
+                    </div>
+                </div>
+                <div class="card-resume-values">
+                    <span>R$ 1000</span>
+                </div>
+            </div>
+
+            <div class="card-resume">
+                <div class="card-resume-title">
+                    <span>Entradas</span>
+                    <div class='icon'>
+                        <i class="fa-solid fa-dollar-sign"></i>
+                    </div>
+                </div>
+                <div class="card-resume-values">
+                    <span>R$ 1000</span>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -23,17 +58,38 @@ export default {
 }
 
 .content {
-    width: 60%;
+    width: 70%;
     display: flex;
     justify-content: space-around;
     gap: 20px;
 }
 
 .card-resume {
-    width: 40%;
+    width: 50%;
     height: 120px;
     background-color: #FFF;
     margin-top: -50px;
     border-radius: 8px;
+}
+
+.card-resume-title {
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    height: 60%;
+}
+
+.card-resume-values {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.icon {
+    width: 20px;
+    border: 1px solid black;
+    border-radius: 20px;
+    padding: 2px;
+    text-align: center;
 }
 </style>
